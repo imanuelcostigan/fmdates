61808296fa26909a1b7734d72f15c53b7086f2a9
Imanuel Costigan <i.costigan@me.com>
2017-01-05 20:07:53 +1100

    Document calendar classes

--- R/calendar-classes.R
+++ R/calendar-classes.R
@@ -1,3 +1,30 @@
+#' Build a calendar
+#'
+#' Calendars are necessary for two reasons: they define whether a calendar day
+#' is a good business day in a given locale and they are used to store the time
+#' zone for the locale. Calendars can correspond to a single locale (usually a
+#' city). These inherit from the `Calendar` class. The package implements a
+#' number of calendars for key financial market locales such as
+#' `AUSYCalendar`, `USNYCalendar` and `EUTACalendar` (TARGET). You can
+#' also define a joint locale using [JointCalendar()].
+#'
+#' @param locale a four letter string representing an abbreviation of the
+#'   locale. The package uses locale representations loosely based on
+#'   [UN/LOCODE](http://www.unece.org/cefact/locode/welcome.html) (e.g.
+#'   Australia/Sydney is represented by `AUSY` rather than `AU/SYD` per the
+#'   LOCODE specification). The locale is used as a prefix to the calendar's
+#'   S3 class in the following manner: `<locale>Calendar` (e.g. `AUSYCalendar`).
+#' @param tz the time zone associated with the given `locale` using
+#'   [OlsonNames()] (e.g. `Australia/Sydney`)
+#' @return `Calendar()` returns a function that constructs an object inheriting
+#'   from the `Calendar` class. The calendar constructors provided by the
+#'   package returns an object that inherits from `Calendar`.
+#' @examples
+#' Calendar(NA, NA) # Defined: EmptyCalendar()
+#' Calendar("AUSY", "Australia/Sydney") # Defined: AUSYCalendar()
+#' @export
+#' @family calendar classes
+
 Calendar <- function(locale, tz) {
   function () {
     structure(list(locale = locale, tz = tz),
@@ -5,20 +32,48 @@ Calendar <- function(locale, tz) {
   }
 }
 
+#' @rdname Calendar
 EmptyCalendar <- Calendar(NA, NA)
+#' @rdname Calendar
 AUSYCalendar <- Calendar("AUSY", "Australia/Sydney")
+#' @rdname Calendar
 AUMECalendar <- Calendar("AUME", "Australia/Melbourne")
+#' @rdname Calendar
 CHZHCalendar <- Calendar("CHZH", "Europe/Zurich")
+#' @rdname Calendar
 EUTACalendar <- Calendar("EUTA", "Europe/Brussels")
+#' @rdname Calendar
 GBLOCalendar <- Calendar("GBLO", "Europe/London")
+#' @rdname Calendar
 HKHKCalendar <- Calendar("HKHK", "Asia/Hong_Kong")
+#' @rdname Calendar
 JPTOCalendar <- Calendar("JPTO", "Asia/Tokyo")
+#' @rdname Calendar
 NOOSCalendar <- Calendar("NOOS", "Europe/Oslo")
+#' @rdname Calendar
 NZAUCalendar <- Calendar("NZAU", "Pacific/Auckland")
+#' @rdname Calendar
 NZWECalendar <- Calendar("NZWE", "Pacific/Auckland")
+#' @rdname Calendar
 USNYCalendar <- Calendar("USNY", "America/New_York")
 
 
+#' Joint calendars
+#'
+#' Sometimes the calendar governing a financial contract is defined by multiple
+#' single locales. These joint calendars are represented by the `JointCalendar`
+#' class.
+#'
+#'
+#' @param calendars a list of at least one `Calendar()` objects
+#' @param rule either `all` or `any` corresponding to a date being good if
+#' it is good in all or any of the calendars supplied.
+#' @return an object of class `JointCalendar` when using `JointCalendar()`
+#' @examples
+#' JointCalendar(list(AUSYCalendar(), AUMECalendar()), all)
+#' JointCalendar(list(AUSYCalendar(), AUMECalendar()), any)
+#' @export
+#' @family calendar classes
 JointCalendar <- function(calendars, rule = all) {
   assertthat::assert_that(is_list_of(calendars, "Calendar"))
   locales <- vapply(calendars, locale, "character")
@@ -27,7 +82,12 @@ JointCalendar <- function(calendars, rule = all) {
   tzs <- vapply(calendars, tz, "character")[!is_duplicated]
   calendars <- calendars[!is_duplicated]
   structure(
-    list(locales = locales, tzs = tzs, calendars = calendars, rule = rule),
+    list(
+      locales = locales,
+      tzs = tzs,
+      calendars = calendars,
+      rule = rule
+    ),
     class = "JointCalendar"
   )
 }
