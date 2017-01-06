c94b1a599208198f7d871176379c9c5751134650
Imanuel Costigan <i.costigan@me.com>
2017-01-06 17:01:16 +1100

    Removing functions not ready for prime time
    * Keep initial scope of package limited to calendars etc
    * Work out where these functions sit in package ecosystem

--- tests/testthat/test-date-methods.R
+++ tests/testthat/test-date-methods.R
@@ -8,35 +8,8 @@ test_that("is_eom works as expected", {
   expect_identical(is_eom(ymd("2012-02-29")), TRUE)
 })
 
-test_that("nth_nday works as expected", {
-  expect_identical(nth_nday(ymd("20120601"), 2, 6), ymd("20120608"))
-  expect_identical(nth_nday(ymd("20120601"), 2, "fri"), ymd("20120608"))
-  expect_identical(nth_nday(ymd("20120601"), 2, "Fri"), ymd("20120608"))
-  expect_identical(nth_nday(ymd("20120901"), 3, 4), ymd("20120919"))
-  expect_identical(nth_nday(ymd("20121201"), 1, 2), ymd("20121203"))
-  expect_identical(nth_nday(ymd("20121215"), 4, 5), ymd("20130110"))
-  expect_identical(nth_nday(ymd("20121102"), 14, 3), ymd("20130205"))
-})
-
-test_that("futures_settlement:", {
-  d1 <- lubridate::as_date(ymd(20160101))
-  expect_identical(futures_settlement(d1, 1, 2, "fri"),
-    lubridate::as_date(ymd(20160311)))
-  expect_identical(futures_settlement(d1, 2, 2, "fri"),
-    lubridate::as_date(ymd(20160610)))
-  expect_identical(futures_settlement(d1, 1, 1, "wed", 9),
-    lubridate::as_date(ymd(20160316)))
-  expect_identical(futures_settlement(d1, 2, 1, "wed", 9),
-    lubridate::as_date(ymd(20160615)))
-  # Waiting to reimplement calendars
-  # expect_identical(futures_settlement(d1, 1, 3, "wed", post_offset = days(-1),
-  #   post_offset_calendar = CHZHCalendar$new()),
-  #   lubridate::as_date(ymd(20160315)))
-})
-
 test_that("eom works as expected", {
-  # Waiting to reimplement calendars
-  # expect_identical(eom(ymd(20120203, 20140203)), ymd(20120229, 20140228))
+  expect_identical(eom(ymd(20120203, 20140203)), ymd(20120229, 20140228))
 })
 
 test_that("Julian day works:", {
