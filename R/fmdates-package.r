c65f6ccc496e4b5707371ba016f0159a36e90354
Imanuel Costigan <i.costigan@me.com>
2017-01-07 11:19:15 +1100

    Improving package doc

--- R/fmdates-package.r
+++ R/fmdates-package.r
@@ -1,8 +1,12 @@
 #' fmdates
 #'
 #' Implements common date calculations relevant for specifying the economic
-#' nature of financial market contracts and typically defined in ISDA legal
-#' documentation.
+#' nature of financial market contracts that are typically defined by
+#' International Swap Dealer Association (ISDA) legal documentation.
+#'
+#' The key classes and methods introduced by this package are documented in
+#' [`Calendar`], [`JointCalendar`], [is_good()], [adjust()], [shift()] and
+#' [year_frac()].
 #'
 #' @name fmdates
 #' @docType package
